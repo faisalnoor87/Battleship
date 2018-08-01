e4d4d8282e339581a1cd46c46a2627068f0d7741
Faisal Noor <faisal.noor@myob.com>
2018-08-01 12:33:31 +1200

    remove unused variables

--- Battleship.Core/Console.cs
+++ Battleship.Core/Console.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
-using System.Linq;
 
 namespace Battleship.Core
 {
--- Battleship.Core/Storage.cs
+++ Battleship.Core/Storage.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Battleship.Core
+﻿namespace Battleship.Core
 {
     public static class Storage
     {
